ae432d65b39b97e39dc6a2bfed31d3051c926d66
AlVaRo! <AlVaRo!@ALVARO-PC>
2021-03-28 13:52:41 +0200

    Agregada la funcion de sumar, restar, multiplicar y dividir.

--- src/git/gitPrueba.java
+++ src/git/gitPrueba.java
@@ -8,11 +8,21 @@ public class gitPrueba {
 		gitPrueba b = new gitPrueba();
 		b.setReal(4.6);
 		b.setImag(3.5);
+		gitPrueba suma = a.sumar(b);
+		gitPrueba resta = a.restar(b);
+		gitPrueba mult = a.multiplicar(b);
+		gitPrueba mult2 = a.multiplicar(5.2);
+		gitPrueba div = a.dividir(b);
+		System.out.println(suma);
+		System.out.println(resta);
+		System.out.println(mult);
+		System.out.println(mult2);
+		System.out.println(div);
 		if (a.equals(b))
 			System.out.println("Los dos números son iguales");
 		
 		
-	
+		
 		
 	}
 	private double real;
@@ -51,7 +61,55 @@ public class gitPrueba {
 		this.imag = imag;
 	}
 	
-	
+
+
+
+	//Métodos públicos
+		public gitPrueba sumar(gitPrueba num) {
+			gitPrueba aux = new gitPrueba();
+
+			aux.setReal(real + num.real);
+			aux.setImag(imag + num.imag);
+
+			return aux;
+		}
+		
+		public gitPrueba restar(gitPrueba num) {
+			gitPrueba aux = new gitPrueba();
+			
+			aux.setReal(this.real - num.real);
+			aux.setImag(imag - num.imag);
+			
+			return aux;
+		}
+		
+		public gitPrueba multiplicar(gitPrueba num) {
+			gitPrueba aux = new gitPrueba();
+			
+			aux.setReal(real * num.real - imag * num.imag);
+			aux.setImag(real * num.imag + imag * num.real);
+			
+			return aux;
+		}
+		
+		public gitPrueba multiplicar(double num) {
+			gitPrueba aux = new gitPrueba();
+			
+			aux.setReal(real * num);
+			aux.setImag(imag * num);
+
+			return aux;
+		}
+		
+		public gitPrueba dividir(gitPrueba num) {
+			gitPrueba aux = new gitPrueba();
+			
+			aux.setReal((real * num.real + imag * num.imag)/(num.real * num.real + num.imag * num.imag));
+			aux.setImag((imag * num.real - real * num.imag)/(num.real * num.real + num.imag * num.imag));
+			
+			return aux;
+		}
+		
 }
 
 
