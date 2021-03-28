0fa1bd443d61f8ab247d55f2fbb33c98686be8a2
AlVaRo! <AlVaRo!@ALVARO-PC>
2021-03-28 13:55:09 +0200

    Proyecto terminado

--- src/git/gitPrueba.java
+++ src/git/gitPrueba.java
@@ -21,8 +21,8 @@ public class gitPrueba {
 		if (a.equals(b))
 			System.out.println("Los dos números son iguales");
 		
-		
-		
+		gitPrueba pot = gitPrueba.potencia(a,3);
+		System.out.println(pot);
 		
 	}
 	private double real;
@@ -109,7 +109,85 @@ public class gitPrueba {
 			
 			return aux;
 		}
+
+		public static gitPrueba potencia(gitPrueba num, int exponente) {
+			gitPrueba aux = new gitPrueba();
+			
+			double real = 0;
+			double imag = 0;
+			int esPar;
+			
+			for (int i = 0; i <= exponente; i++) {
+				esPar = (i % 2 == 0) ? 1 : -1;
+				
+				real += combinatorio(exponente, 2 * i) * potencia(num.real, exponente - 2 * i) * potencia(num.imag, 2 * i) * esPar;
+				
+				if (exponente == 2 * i) break;
+				
+				imag += combinatorio(exponente, 2 * i + 1) * potencia(num.real, exponente - (2 * i + 1)) * potencia(num.imag, 2 * i + 1) * esPar;
+			}
+			
+			aux.setReal(real);
+			aux.setImag(imag);
+			
+			return aux;
+		}
 		
+		// Métodos privados
+		private static double combinatorio(int m, int n) {
+			long num = 1;
+			long den = 1;
+			
+			for (int i = m; i > m - n; i--) {
+				num = num * i;
+			}
+			
+			for (int i = 2; i <= n; i++) {
+				den = den * i;
+			}
+			
+			return num / den;
+		}
+		
+		private static double potencia(double base, int exponente) {
+			double resultado = 1;
+			
+			for (int i = 0; i < exponente; i++) {
+				resultado = resultado * base;
+			}
+			
+			return resultado;
+		}
+
+		// Overrides
+		@Override
+		public String toString() {
+			return "(" + real + ", " + imag + ")";
+		}
+		
+		@Override
+		public boolean equals(Object obj) {
+			if (obj == null) {
+				return false;
+			}
+			
+			if (getClass() != obj.getClass()) {
+				return false;
+			}
+			
+			final gitPrueba other = (gitPrueba) obj;
+			
+			if (this.real != other.real) {
+				return false;
+			}
+			
+			if (this.imag != other.imag) {
+				return false;
+			}
+			
+			return false;
+		}
+
 }
 
 
